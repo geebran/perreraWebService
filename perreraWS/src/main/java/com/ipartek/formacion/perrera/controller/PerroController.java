ae13da27848fb01dd3ef60110a580124333ec025
Curso <Curso@FR-TOSH-9.ipartekaula.com>
2016-10-24 16:17:55 +0200

    implementar jersey y primer metodo del cotrolador del Perro

---
+++ perreraWS/src/main/java/com/ipartek/formacion/perrera/controller/PerroController.java
@@ -0,0 +1,63 @@
+package com.ipartek.formacion.perrera.controller;
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import java.util.ArrayList;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+
+import com.ipartek.formacion.perrera.dao.PerroDAOImpl;
+import com.ipartek.formacion.perrera.pojo.Perro;
+
+
+/**
+ * El poryecto hace refencia al proyecto skalada
+ *
+ * @author Curso
+ *
+ */
+@Path("/perro")
+@Api(value = "/perro")
+public class PerroController {
+	
+	
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Listado de Perros", notes = "Listado de perros existentes en la perrera, limitado a 1.000", response = Perro.class, responseContainer = "List")
+
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Todo OK"),
+			@ApiResponse(code = 500, message = "Error inexperado en el servidor") })
+	public Response getAll(
+			@ApiParam(name = "orderBy", required = false, value = "Filtro para ordenar los perros de forma ascendente o descendente, posibles valores [asc|desc]") @DefaultValue("asc") @QueryParam("orderBy") String orderBy,
+			@ApiParam(name = "campo", required = false, value = "Filtro para ordenar por 'campo' los perros, posibles valores [id|nombre|raza]") @DefaultValue("id")  @QueryParam("campo") String campo) {
+		try {
+			
+			PerroDAOImpl dao = PerroDAOImpl.getInstance();
+			ArrayList<Perro> perros = (ArrayList<Perro>) dao.getAll(orderBy, campo);			
+			return Response.ok().entity(perros).build();
+			
+		} catch (Exception e) {
+			return Response.serverError().build();
+		}
+	}
+	
+}
